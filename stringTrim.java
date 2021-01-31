f6322a68c10f645a1ec0355b6cbf4800e5c3bb42
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-01-31 23:37:26 +0530

    String Trim
    The above code shows how to trim spaces in the given string. The trim() function removes all kind of space characters at both ends, means removes starting and trailing spaces. These space characters includes normal space, enter, new line, tab, etc.

---
+++ stringTrim.java
@@ -0,0 +1,8 @@
+package com.myjava.string;
+ 
+public class MyStringTrim {
+    public static void main(String a[]){
+        String str = "  Junk   ";
+        System.out.println(str.trim());
+    }
+}
